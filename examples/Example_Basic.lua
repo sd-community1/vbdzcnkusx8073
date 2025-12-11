9e31298e25dce3a177a520fd9f948de1e353432a
sd-community1 <xvvv99634@gmail.com>
2025-12-11 19:29:04 +0300

    Create Example_Basic.lua

---
+++ examples/Example_Basic.lua
@@ -0,0 +1,9 @@
+-- مثال الاستخدام الأساسي
+local HUD = loadstring(game:HttpGet("https://raw.githubusercontent.com/username/Roblox-Beautiful-HUD/main/loader.lua"))()
+
+HUD:Init({
+    theme = "Dark",
+    position = "TopRight",
+    showFPS = true,
+    showTime = true
+})
